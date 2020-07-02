2978eb3911fff3f731310ee0e21e291def3c301d
sapphire-pt <sapphire_pt@sappcoin.com>
2020-07-02 16:17:48 +0100

    version change

--- depends/packages/libXext.mk
+++ depends/packages/libXext.mk
@@ -1,5 +1,5 @@
 package=libXext
-$(package)_version=1.3.2
+$(package)_version=1.3.2.1
 $(package)_download_path=https://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=f829075bc646cdc085fa25d98d5885d83b1759ceb355933127c257e8e50432e0
